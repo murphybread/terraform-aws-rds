31d4e6f5d88cf789f61973c8c927798f33758f06
김민찬 <minchan@megazone.com>
2023-08-12 13:31:29 +0900

    moudle - rds

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_db_instance" "education" {
+  identifier             = "${var.db_name}-${random_pet.random.id}"
+  instance_class         = var.db_instance_class # 修正
+  allocated_storage      = 5
+  engine                 = "postgres"
+  engine_version         = "14.1"
+
+variable "db_name" {
+  description = "Unique name to assign to RDS instance"
+  default = "t101"
+}
+
+variable "db_instance_class" {
+  type        = string
+  description = "The type of database instance to create"
+}
